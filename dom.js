8012209a2e0e768ec6f801c0981e7fc826f3d700
Sazedur Khan <fahim123810@gmail.com>
2024-08-12 19:00:24 +0000

    description of changes

--- dom.js
+++ dom.js
@@ -3,3 +3,19 @@ const darkModeToggle = document.getElementById('darkModeToggle');
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+const learnMoreToggle = document.getElementById('learnMoreToggle');
+const description = document.getElementById('description');
+
+if (learnMoreToggle) {
+    learnMoreToggle.addEventListener('click', () => {
+        if (description.style.display === 'none' || description.style.display === '') {
+            description.style.display = 'grid';
+            learnMoreToggle.textContent = 'Go Back';
+        } else {
+            description.style.display = 'none';
+            learnMoreToggle.textContent = 'Learn More';
+        }
+    });
+}
+
